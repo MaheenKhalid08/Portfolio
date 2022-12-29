acd02d5b26309c2725676e298ecca3f4e3366db7
MaheenKhalid08 <50275260+MaheenKhalid08@users.noreply.github.com>
2022-12-28 20:39:38 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ import {
   Navigate
 } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
-import "./style.css";
-import "./App.css";
+import "./Home/style.css";
+import "./Home/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
